f53f13da96db2b742ffd8c6beb2d578a4ff5dadf
Sergey Kiryushichev <s_kiryushichev@wargaming.net>
2017-03-27 15:17:10 +0300

    DF-12708: renamed input control to input element
    Former-commit-id: 5aade113cd094d89b7d45d2b1dfb8843358ee32a

--- Programs/TestBed/Classes/Tests/InputSystemTest.h
+++ Programs/TestBed/Classes/Tests/InputSystemTest.h
@@ -17,7 +17,7 @@ protected:
     void UnloadResources() override;
 
 private:
-    DAVA::UIButton* CreateKeyboardUIButton(DAVA::eInputControl key, DAVA::WideString text, DAVA::FTFont* font, DAVA::float32 x, DAVA::float32 y, DAVA::float32 w, DAVA::float32 h);
+    DAVA::UIButton* CreateKeyboardUIButton(DAVA::eInputElements key, DAVA::WideString text, DAVA::FTFont* font, DAVA::float32 x, DAVA::float32 y, DAVA::float32 w, DAVA::float32 h);
     bool OnInputEvent(DAVA::InputEvent const& event);
     void OnBeginFrame();
     void OnAction(DAVA::Action action);
