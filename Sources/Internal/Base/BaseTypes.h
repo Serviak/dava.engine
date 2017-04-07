d1cfa512b829c3a106f5fae33b5a693beb140fbe
Aleksey Podverbnyy <a_podverbnyy@wgs-macbook-pro.corp.wargaming.local>
2017-04-07 12:25:38 +0300

    DF-12606 corrected expr.eval to compile on iOS;
    Former-commit-id: a080f08bac19420399b84d9acd35acc36eada304

--- Sources/Internal/Base/BaseTypes.h
+++ Sources/Internal/Base/BaseTypes.h
@@ -128,6 +128,14 @@ inline T Clamp(T val, T a, T b)
 #define Snprintf snprintf
 #endif
 
+
+#if defined(__DAVAENGINE_WINDOWS__)
+#define strnicmp _strnicmp 
+#elif defined(__DAVAENGINE_MACOS__) || defined(__DAVAENGINE_IPHONE__) || defined(__DAVAENGINE_ANDROID__)
+#define strnicmp strncasecmp 
+#else
+#endif
+
 template <class TYPE>
 void SafeDelete(TYPE*& d)
 {
