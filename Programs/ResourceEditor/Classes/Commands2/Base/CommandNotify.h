61ca499eaeb124c3a2d0654a855afe1bb9a93144
y_rakhuba <y_rakhuba@wargaming.net>
2017-04-17 09:19:27 +0300

    [DF-13316] SlotSystem integration into RE
    Former-commit-id: 511984a7693dbbc5e9d116843e035942372acbbe

--- Programs/ResourceEditor/Classes/Commands2/Base/CommandNotify.h
+++ Programs/ResourceEditor/Classes/Commands2/Base/CommandNotify.h
@@ -4,9 +4,11 @@
 #include "Base/BaseObject.h"
 
 class RECommandNotificationObject;
+class REDependentCommandsHolder;
 class CommandNotify : public DAVA::BaseObject
 {
 public:
+    virtual void AccumulateDependentCommands(REDependentCommandsHolder& holder) = 0;
     virtual void Notify(const RECommandNotificationObject& commandNotification) = 0;
     virtual void CleanChanged(bool clean);
     virtual void CanUndoChanged(bool canUndo);
@@ -23,6 +25,7 @@ public:
     void SetNotify(CommandNotify* notify);
     CommandNotify* GetNotify() const;
 
+    void AccumulateDependentCommands(REDependentCommandsHolder& holder);
     void EmitNotify(const RECommandNotificationObject& commandNotification);
     void EmitCleanChanged(bool clean);
     void CanUndoChanged(bool canUndo);
