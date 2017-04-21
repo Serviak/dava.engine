c96a5cc75b004f987f535dc5e0e14c2ab731a09c
Aleksey Podverbnyy <a_podverbnyy@wargaming.net>
2017-04-21 16:18:51 +0300

    DF-12606 minor corrections;
    Former-commit-id: 6548f9725b5ccc11d05469cc924a663ed2970baf

--- Sources/Internal/Render/RHI/Common/rhi_ShaderSource.cpp
+++ Sources/Internal/Render/RHI/Common/rhi_ShaderSource.cpp
@@ -1965,7 +1965,6 @@ std::vector<ShaderSourceCache::entry_t> ShaderSourceCache::Entry;
 
 const ShaderSource* ShaderSourceCache::Get(FastName uid, uint32 srcHash)
 {
-    return nullptr;
     LockGuard<Mutex> guard(shaderSourceEntryMutex);
 
     //    Logger::Info("get-shader-src (host-api = %i)",HostApi());
