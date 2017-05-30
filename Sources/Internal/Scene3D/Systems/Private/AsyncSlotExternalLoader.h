1c6a1916c95b57a368f10bd715dd29d83ef3775d
y_rakhuba <y_rakhuba@wargaming.net>
2017-05-30 11:37:41 +0300

    [DF-13316] Doxygen comments and async strategy of items loading
    Former-commit-id: 466cfabe5ad57d07e357ad1832f7a225f60d8db7

---
+++ Sources/Internal/Scene3D/Systems/Private/AsyncSlotExternalLoader.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Scene3D/Systems/SlotSystem.h"
+
+#include "Concurrency/Mutex.h"
+#include "Base/RefPtr.h"
+#include "Base/Hash.h"
+
+namespace DAVA
+{
+class AsyncSlotExternalLoader final : public SlotSystem::ExternalEntityLoader, public std::enable_shared_from_this<AsyncSlotExternalLoader>
+{
+public:
+    Entity* Load(const FilePath& path) override;
+    void AddEntity(Entity* parent, Entity* child) override;
+    void Process(float32 delta) override;
+
+    void LoadImpl(RefPtr<Entity> rootEntity, const FilePath& path);
+
+private:
+    struct HashRefPtrEntity
+    {
+        size_t operator()(const RefPtr<Entity>& pointer) const;
+    };
+
+    UnorderedMap<RefPtr<Entity>, RefPtr<Scene>, HashRefPtrEntity> jobsMap;
+    Mutex mutex;
+};
+
+inline size_t AsyncSlotExternalLoader::HashRefPtrEntity::operator()(const RefPtr<Entity>& pointer) const
+{
+    return reinterpret_cast<size_t>(pointer.Get());
+}
+
+} // namespace DAVA
--- Sources/Internal/Scene3D/Systems/SlotSystem.h
+++ Sources/Internal/Scene3D/Systems/SlotSystem.h
@@ -5,13 +5,30 @@
 #include "Base/RefPtr.h"
 #include "Base/FastName.h"
 
+#include <memory>
+
 namespace DAVA
 {
 class SlotComponent;
-class SlotSystem : public SceneSystem
+
+/**
+    \ingroup systems
+    \brief SlotSystem manage slots (SlotComponent), loaded items and provide methods to load item into slot or into all
+    slots with some name. SlotSystem delegate item loading to ExternalEntityLoader, that can implement some custom logic.
+    For example, hold cache of Entities or add some properties to loaded Entity. By default ExternalEntityLoader implement
+    async loaded strategy. Method AttachItemToSlot will return 'empty Entity' immediately. Loading will process in separate
+    thread. Item will be attached into 'empty Entity' after it will be fully loaded.
+
+    SlotSystem use ItemsCache to hold result of config files parsing. By default SlotSystem use one ItemsCache per scene,
+    but game can override this behaviour and call SetSharedCache to change scope of this cache.
+
+    Config parsing is a lazy operation. In method AttachItemToSlot SlotSystem call ItemsCache to find item with some name.
+    If cache has not parse config file of SlotComponent yet, it will parse this config.
+*/
+class SlotSystem final : public SceneSystem
 {
 public:
-    class ItemsCache : public BaseObject
+    class ItemsCache
     {
     public:
         struct Item
@@ -38,11 +55,12 @@ public:
         UnorderedMap<String, Set<Item, ItemLess>> cachedItems;
     };
 
-    class ExternalEntityLoader : public BaseObject
+    class ExternalEntityLoader
     {
     public:
         virtual Entity* Load(const FilePath& path) = 0;
         virtual void AddEntity(Entity* parent, Entity* child) = 0;
+        virtual void Process(float32 delta) = 0;
 
         void SetScene(Scene* scene);
 
@@ -53,9 +71,18 @@ public:
     SlotSystem(Scene* scene);
     ~SlotSystem();
 
-    void SetSharedCache(RefPtr<ItemsCache> cache);
+    /**
+        Override default ItemsCache. Changing of items cache will produce cache invalidation,
+        but will not produce unloading of loaded items 
+    */
+    void SetSharedCache(std::shared_ptr<ItemsCache> cache);
+    /**
+        Get result of parsing config file. This method can be used to precache config files
+        \return Items that described in \c configPath
+    */
     Vector<ItemsCache::Item> GetItems(const FilePath& configPath);
-    void SetExternalEntityLoader(RefPtr<ExternalEntityLoader> externalEntityLoader);
+    /** Override default async loader */
+    void SetExternalEntityLoader(std::shared_ptr<ExternalEntityLoader> externalEntityLoader);
 
     void UnregisterEntity(Entity* entity) override;
 
@@ -67,24 +94,37 @@ public:
 
     void Process(float32 timeElapsed) override;
 
+    /**
+        Load and attach item with name \c itemName into all slots with name \c slotName recursively in hierarchy with root \c rootEntity
+        \arg \c rootEntity root of hierarchy. Should be attached to scene
+    */
     void AttachItemToSlot(Entity* rootEntity, FastName slotName, FastName itemName);
+    /**
+        Load and attach item with name \c itemName into slots \c component
+        \return Loaded and attached to slot Entity
+    */
     Entity* AttachItemToSlot(SlotComponent* component, FastName itemName);
+    /** Attach \c entity into slot \c component and named it by name equal to \c itemName */
     void AttachEntityToSlot(SlotComponent* component, Entity* entity, FastName itemName);
 
+    /** Lookup entity that currently loaded into slot \c component. Can return nullptr if slot currently empty */
     Entity* LookUpLoadedEntity(SlotComponent* component) const;
+    /** Lookup slot by \c entity that currently loaded into it. Can return nullptr if \c entity is not loaded item*/
     SlotComponent* LookUpSlot(Entity* entity) const;
+    /** Helper function to get local transform of joint that \c component attached to. If \c component doesn't attached to joint, return Identity*/
     Matrix4 GetJointTransform(SlotComponent* component) const;
 
 protected:
     void SetScene(Scene* scene) override;
-    void UnloadItem(SlotComponent* component);
 
 private:
+    void UnloadItem(SlotComponent* component);
+
     UnorderedMap<Component*, Entity*> slotToLoadedEntity;
     UnorderedMap<Entity*, Component*> loadedEntityToSlot;
 
-    RefPtr<ExternalEntityLoader> externalEntityLoader;
-    RefPtr<ItemsCache> sharedCache;
+    std::shared_ptr<ExternalEntityLoader> externalEntityLoader;
+    std::shared_ptr<ItemsCache> sharedCache;
 
     Vector<Entity*> deletePending;
 };
