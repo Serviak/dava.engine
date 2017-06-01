0cd25ddf9043ef479b0e0e050a6445201a08d870
y_rakhuba <y_rakhuba@wargaming.net>
2017-06-01 08:46:56 +0300

    [DF-13316] Queued async loading of items. Resources for unittests
    Former-commit-id: d89bd58abcea651b246bd645abfa0e6d72df1dc9

--- Sources/Internal/Scene3D/Systems/Private/AsyncSlotExternalLoader.cpp
+++ Sources/Internal/Scene3D/Systems/Private/AsyncSlotExternalLoader.cpp
@@ -17,14 +17,21 @@ Entity* AsyncSlotExternalLoader::Load(const FilePath& path)
     RefPtr<Entity> rootEntity;
     rootEntity.ConstructInplace();
     rootEntity->SetName(path.GetBasename().c_str());
-    jobsMap.emplace(rootEntity, RefPtr<Scene>());
 
-    JobManager* jobMng = GetEngineContext()->jobManager;
-    std::shared_ptr<AsyncSlotExternalLoader> loaderRef = shared_from_this();
-    jobMng->CreateWorkerJob([loaderRef, rootEntity, path]() {
-        loaderRef->LoadImpl(rootEntity, path);
-    });
+    {
+        LockGuard<Mutex> guard(jobsMutex);
+        jobsMap.emplace(rootEntity, RefPtr<Scene>());
+    }
 
+    {
+        LockGuard<Mutex> guard(queueMutes);
+        loadingQueue.push_back(LoadTask());
+        LoadTask& task = loadingQueue.back();
+        task.filePath = path;
+        task.rootEntity = rootEntity;
+    }
+
+    ApplyNextJob();
     return rootEntity.Get();
 }
 
@@ -35,7 +42,7 @@ void AsyncSlotExternalLoader::AddEntity(Entity* parent, Entity* child)
 
 void AsyncSlotExternalLoader::Process(float32 delta)
 {
-    LockGuard<Mutex> guard(mutex);
+    LockGuard<Mutex> guard(jobsMutex);
     auto currentIter = jobsMap.begin();
     auto endIter = jobsMap.end();
     while (currentIter != endIter)
@@ -62,7 +69,7 @@ void AsyncSlotExternalLoader::LoadImpl(RefPtr<Entity> rootEntity, const FilePath
 {
 #if defined(__DAVAENGINE_DEBUG__)
     {
-        LockGuard<Mutex> guard(mutex);
+        LockGuard<Mutex> guard(jobsMutex);
         DVASSERT(jobsMap.count(rootEntity) > 0);
     }
 #endif // DEBUG
@@ -73,13 +80,49 @@ void AsyncSlotExternalLoader::LoadImpl(RefPtr<Entity> rootEntity, const FilePath
     if (result != SceneFileV2::ERROR_NO_ERROR)
     {
         Logger::Error("[AsyncSlotExternalLoader] Couldn't load scene %s", path.GetAbsolutePathname().c_str());
-        LockGuard<Mutex> guard(mutex);
+        LockGuard<Mutex> guard(jobsMutex);
         jobsMap.erase(rootEntity);
         return;
     }
 
-    LockGuard<Mutex> guard(mutex);
-    jobsMap[rootEntity] = scene;
+    {
+        LockGuard<Mutex> guard(jobsMutex);
+        jobsMap[rootEntity] = scene;
+    }
+
+    {
+        LockGuard<Mutex> loadingGuard(queueMutes);
+        isLoadingActive = false;
+    }
+
+    ApplyNextJob();
+}
+
+void AsyncSlotExternalLoader::ApplyNextJob()
+{
+    {
+        LockGuard<Mutex> loadingGuard(queueMutes);
+        if (isLoadingActive == true)
+        {
+            return;
+        }
+
+        if (loadingQueue.empty() == true)
+        {
+            return;
+        }
+
+        isLoadingActive = true;
+    }
+
+    JobManager* jobMng = GetEngineContext()->jobManager;
+    std::shared_ptr<AsyncSlotExternalLoader> loaderRef = shared_from_this();
+    LoadTask task = loadingQueue.front();
+    loadingQueue.pop_front();
+    jobMng->CreateWorkerJob([loaderRef, task]()
+                            {
+                                loaderRef->LoadImpl(task.rootEntity, task.filePath);
+                            });
 }
 
 } // namespace DAVA
