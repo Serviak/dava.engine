9c0ca44a82cf062fe98cf18deff5514828b2efab
k_lyashkevich <k_lyashkevich@wargaming.net>
2017-06-16 15:02:29 +0300

    [DF-13832] now user can costumize graphics
    Former-commit-id: 1f0ebba5d5d9bf8a18eb2e6152e9190afc42fb9d

--- Programs/QuickEd/Classes/EditorSystems/HUDControls.h
+++ Programs/QuickEd/Classes/EditorSystems/HUDControls.h
@@ -4,8 +4,51 @@
 #include "EditorSystemsManager.h"
 
 #include <Functional/TrackedObject.h>
+#include <Base/Introspection.h>
+#include <Math/Color.h>
+#include <FileSystem/FilePath.h>
 
 class VisibleValueProperty;
+
+class HUDControlsPreferences : public DAVA::InspBase
+{
+public:
+    HUDControlsPreferences();
+
+#ifdef DECLARE_PREFERENCE
+#error "DECLARE_PREFERENCE is already declared";
+#endif //DECLARE_PREFERENCE
+
+#define DECLARE_PREFERENCE(T, pref) \
+    static T pref; \
+    T Get##pref() const; \
+    void Set##pref(const T& pref);
+
+    DECLARE_PREFERENCE(DAVA::Color, selectionRectColor);
+    DECLARE_PREFERENCE(DAVA::Color, highlightColor);
+    DECLARE_PREFERENCE(DAVA::Color, hudRectColor);
+    DECLARE_PREFERENCE(DAVA::FilePath, cornerRectPath);
+    DECLARE_PREFERENCE(DAVA::FilePath, borderRectPath);
+    DECLARE_PREFERENCE(DAVA::FilePath, pivotPointPath);
+    DECLARE_PREFERENCE(DAVA::FilePath, rotatePath);
+    DECLARE_PREFERENCE(DAVA::FilePath, magnetLinePath);
+    DECLARE_PREFERENCE(DAVA::FilePath, magnetRectPath);
+
+#undef DECLARE_PREFERENCE
+    //old introspection dont compile on DAVA::Color and DAVA::FilePath as member
+public:
+    INTROSPECTION(HUDControlsPreferences,
+                  PROPERTY("selectionRectColor", "User graphic/Selection rect color", GetselectionRectColor, SetselectionRectColor, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("highlightColor", "User graphic/Highlight color", GethighlightColor, SethighlightColor, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("hudRectColor", "User graphic/Frame rect color", GethudRectColor, SethudRectColor, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("cornerRectPath", "User graphic/Corner rect path", GetcornerRectPath, SetcornerRectPath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("borderRectPath", "User graphic/Border rect path", GetborderRectPath, SetborderRectPath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("pivotPointPath", "User graphic/Pivot point control path", GetpivotPointPath, SetpivotPointPath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("rotatePath", "User graphic/Rotate control path", GetrotatePath, SetrotatePath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("magnetLinePath", "User graphic/Magnet line path", GetmagnetLinePath, SetmagnetLinePath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  PROPERTY("magnetRectPath", "User graphic/Magnet rect path", GetmagnetRectPath, SetmagnetRectPath, DAVA::I_SAVE | DAVA::I_VIEW | DAVA::I_EDIT | DAVA::I_PREFERENCE)
+                  )
+};
 
 class ControlContainer : public DAVA::UIControl
 {
@@ -54,13 +97,15 @@ public:
     enum eType
     {
         SELECTION,
-        HIGHLIGHT
+        HIGHLIGHT,
+        SELECTION_RECT
     };
-    explicit FrameControl(eType type = SELECTION);
+    explicit FrameControl(eType type);
 
 protected:
     ~FrameControl() = default;
     void InitFromGD(const DAVA::UIGeometricData& geometricData) override;
+    void SetRect(const DAVA::Rect& rect) override;
     DAVA::Rect GetSubControlRect(const DAVA::Rect& rect, eBorder border) const;
 
     eType type = SELECTION;
@@ -103,4 +148,4 @@ private:
 void SetupHUDMagnetLineControl(DAVA::UIControl* control);
 void SetupHUDMagnetRectControl(DAVA::UIControl* control);
 
-DAVA::RefPtr<DAVA::UIControl> CreateHUDRect(const ControlNode* node);
+DAVA::RefPtr<DAVA::UIControl> CreateHighlightRect(const ControlNode* node);
