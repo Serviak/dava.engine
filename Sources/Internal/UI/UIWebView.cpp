a1f25da9df024aec682686773164fde630e01915
Maksim Lazarev <m_lazarev@wargaming.net>
2017-06-21 17:24:55 +0300

    DF-14086 replace UWP suffix with Win10
    Former-commit-id: 372b733d95fd6c96d2b8556ec5dca431398b77d9

--- Sources/Internal/UI/UIWebView.cpp
+++ Sources/Internal/UI/UIWebView.cpp
@@ -15,7 +15,7 @@
 #elif defined(__DAVAENGINE_IPHONE__)
 #include "UI/Private/iOS/WebViewControliOS.h"
 #elif defined(__DAVAENGINE_WIN_UAP__)
-#include "UI/Private/UWP/WebViewControlUWP.h"
+#include "UI/Private/Win10/WebViewControlWin10.h"
 #elif defined(__DAVAENGINE_ANDROID__)
 #include "UI/Private/Android/WebViewControlAndroid.h"
 #else
