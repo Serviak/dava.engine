f5075a9188a24e3e9d0bf6b7a3803dfd414bbe27
Aliaksandr Valchkevich <a_valchkevich@wargaming.net>
2017-06-26 17:16:18 +0300

    DF-13917 TestBed compilation
    Former-commit-id: 8862a9747d296b9c363432c4d673ed16a7dd8449

--- Programs/TestBed/android/src/com/dava/testbed/TestBed.java
+++ Programs/TestBed/android/src/com/dava/testbed/TestBed.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.content.Intent;
+import android.content.res.Configuration;
 
 import com.dava.engine.DavaActivity;
 
