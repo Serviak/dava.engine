eea5341d34412556884d7cb91e9ba712d08c4980
Aleksandr Vorontsov <a_vorontsov@wargaming.net>
2017-06-30 11:55:26 +0300

    DF-13523 Soft stripe top edge. Default particle value initialization.
    Former-commit-id: 5c5365c691bf31b786c0bc5a2b110453cf57bfa7

--- Sources/Internal/Particles/Particle.h
+++ Sources/Internal/Particles/Particle.h
@@ -12,11 +12,13 @@ struct StripeNode
     float32 lifeime = 0.0f;
     Vector3 position = {};
     Vector3 speed = {};
+    float32 distanceFromBase = 0.0f;
 
-    StripeNode(float32 lifetime_, Vector3 position_, Vector3 speed_)
+    StripeNode(float32 lifetime_, Vector3 position_, Vector3 speed_, float32 distanceFromBase_)
         : lifeime(lifetime_)
         , position(position_)
         , speed(speed_)
+        , distanceFromBase(distanceFromBase_)
     {
     }
 
@@ -38,44 +40,44 @@ struct Particle
 {
     IMPLEMENT_POOL_ALLOCATOR(Particle, 1000);
 
-    Particle* next;
+    Particle* next = nullptr;
 
-    float32 life;
-    float32 lifeTime;
+    float32 life = 0.0f;
+    float32 lifeTime = 0.0f;
 
-    Vector3 position;
-    Vector3 speed;
+    Vector3 position = {};
+    Vector3 speed = {};
 
-    float32 angle;
-    float32 spin;
+    float32 angle = 0.0f;
+    float32 spin = 0.0f;
 
-    int32 frame;
-    float32 animTime;
+    int32 frame = 0;
+    float32 animTime = 0.0f;
 
-    float32 baseFlowSpeed;
-    float32 currFlowSpeed;
-    float32 baseFlowOffset;
-    float32 currFlowOffset;
+    float32 baseFlowSpeed = 0.0f;
+    float32 currFlowSpeed = 0.0f;
+    float32 baseFlowOffset = 0.0f;
+    float32 currFlowOffset = 0.0f;
 
-    float32 baseNoiseScale;
-    float32 currNoiseScale;
-    float32 baseNoiseUScrollSpeed;
-    float32 currNoiseUOffset;
-    float32 baseNoiseVScrollSpeed;
-    float32 currNoiseVOffset;
+    float32 baseNoiseScale = 0.0f;
+    float32 currNoiseScale = 0.0f;
+    float32 baseNoiseUScrollSpeed = 0.0f;
+    float32 currNoiseUOffset = 0.0f;
+    float32 baseNoiseVScrollSpeed = 0.0f;
+    float32 currNoiseVOffset = 0.0f;
 
-    float32 fresnelToAlphaPower;
-    float32 fresnelToAlphaBias;
+    float32 fresnelToAlphaPower = 0.0f;
+    float32 fresnelToAlphaBias = 0.0f;
 
-    float32 currRadius; //for bbox computation
-    float32 alphaRemap;
-    Vector2 baseSize, currSize;
+    float32 currRadius = 0.0f; //for bbox computation
+    float32 alphaRemap = 0.0f;
+    Vector2 baseSize = {}, currSize = {};
 
-    Color color;
+    Color color = {};
 
-    int32 positionTarget; //superemitter particles only
+    int32 positionTarget = 0; //superemitter particles only
 
-    StripeData stripe;
+    StripeData stripe = {};
 };
 }
 
