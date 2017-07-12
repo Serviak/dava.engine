4b55d6c267b86a289dcf8252adc9db2c5e1af639
y_rakhuba <y_rakhuba@wargaming.net>
2017-07-12 17:40:33 +0300

    [DF-14224] Bugfix
    Former-commit-id: dbe091d263ce35ef05e70a2280978fb17a04a389

--- Modules/TArc/Sources/TArc/Controls/PropertyPanel/Private/ReflectedPropertyModel.h
+++ Modules/TArc/Sources/TArc/Controls/PropertyPanel/Private/ReflectedPropertyModel.h
@@ -38,6 +38,7 @@ public:
     bool setData(const QModelIndex& index, const QVariant& value, int role /* = Qt::EditRole */) override;
     QVariant headerData(int section, Qt::Orientation orientation, int role /* = Qt::DisplayRole */) const override;
     Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QModelIndex buddy(const QModelIndex& index) const override;
 
     QModelIndex index(int row, int column, const QModelIndex& parent) const override;
     QModelIndex parent(const QModelIndex& index) const override;
